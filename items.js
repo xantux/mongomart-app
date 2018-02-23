070ab629d12c4c1387262fcd6f5cf8e2570308dd
Santo Antonio Terranova <s.terranova81@gmail.com>
2018-02-23 16:56:04 +0100

    Completed addReview method

--- items.js
+++ items.js
@@ -58,7 +58,6 @@ function ItemDAO(database) {
 
 
   this.getItems = (category, page, itemsPerPage, callback) => {
-
     "use strict";
 
     var query = {
@@ -104,7 +103,6 @@ function ItemDAO(database) {
 
 
   this.searchItems = (query, page, itemsPerPage, callback) => {
-
     "use strict";
 
     var queryObj = {
@@ -170,38 +168,21 @@ function ItemDAO(database) {
   };
 
 
-  this.addReview = function(itemId, comment, name, stars, callback) {
+  this.addReview = (itemId, comment, name, stars, callback) => {
     "use strict";
-
-    /*
-     * TODO-lab4
-     *
-     * LAB #4: Implement addReview().
-     *
-     * Using the itemId parameter, update the appropriate document in the
-     * "item" collection with a new review. Reviews are stored as an
-     * array value for the key "reviews". Each review has the fields:
-     * "name", "comment", "stars", and "date".
-     *
-     */
 
     var reviewDoc = {
       name: name,
       comment: comment,
       stars: stars,
       date: Date.now()
-    }
-
-    // TODO replace the following two lines with your code that will
-    // update the document with a new review.
-    var doc = this.createDummyItem();
-    doc.reviews = [reviewDoc];
-
-    // TODO Include the following line in the appropriate
-    // place within your code to pass the updated doc to the
-    // callback.
-    callback(doc);
-  }
+    };
+  
+    this.db.collection('item').updateOne({ "_id": itemId }, { $push: { "reviews": reviewDoc } }, (err, item) => {
+      assert.equal(err, null);
+      callback(item);
+    });
+  };
 
 
   this.createDummyItem = function() {
