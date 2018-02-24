d04b391564261fce8a7bbcf18e75252a25af9272
Santo Antonio Terranova <s.terranova81@gmail.com>
2018-02-24 14:12:17 +0100

    Removed unnecessary comments and refactored addItem method

--- cart.js
+++ cart.js
@@ -55,56 +55,9 @@ function CartDAO(database) {
     }
 
 
-    /*
-     * This solution is provide as an example to you of several query
-     * language features that are valuable in update operations.
-     * This method adds the item document passed in the item parameter to the
-     * user's cart. Note that this solution works regardless of whether the
-     * cart already contains items or is empty. addItem will be called only
-     * if the cart does not already contain the item. The route handler:
-     * router.post("/user/:userId/cart/items/:itemId"...
-     * handles this. Please review how that method works to have a complete
-     * understanding of how addItem is used.
-     *
-     * NOTE: One may use either updateOne() or findOneAndUpdate() to
-     * write this method. We did not discuss findOneAndUpdate() in class,
-     * but it provides a very straightforward way of solving this problem.
-     * See the following for documentation:
-     * http://mongodb.github.io/node-mongodb-native/2.0/api/Collection.html#findOneAndUpdate
-     *
-     */
-    this.addItem = function(userId, item, callback) {
+    this.addItem = (userId, item, callback) => {
         "use strict";
 
-        // Will update the first document found matching the query document.
-        this.db.collection("cart").findOneAndUpdate(
-            // query for the cart with the userId passed as a parameter.
-            {userId: userId},
-            // update the user's cart by pushing an item onto the items array
-            {"$push": {items: item}},
-            // findOneAndUpdate() takes an options document as a parameter.
-            // Here we are specifying that the database should insert a cart
-            // if one doesn't already exist (i.e. "upsert: true") and that
-            // findOneAndUpdate() should pass the updated document to the
-            // callback function rather than the original document
-            // (i.e., "returnOriginal: false").
-            {
-                upsert: true,
-                returnOriginal: false
-            },
-            // Because we specified "returnOriginal: false", this callback
-            // will be passed the updated document as the value of result.
-            function(err, result) {
-                assert.equal(null, err);
-                // To get the actual document updated we need to access the
-                // value field of the result.
-                callback(result.value);
-            });
-
-        /*
-
-          Without all the comments this code looks written as follows.
-
         this.db.collection("cart").findOneAndUpdate(
             {userId: userId},
             {"$push": {items: item}},
@@ -112,12 +65,10 @@ function CartDAO(database) {
                 upsert: true,
                 returnOriginal: false
             },
-            function(err, result) {
+            (err, result) => {
                 assert.equal(null, err);
                 callback(result.value);
             });
-        */
-
     };
 
 
